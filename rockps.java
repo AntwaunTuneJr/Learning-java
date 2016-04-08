ef676b6e16d441359ac12e927ad2072f0e732394
Antwaun Tune Jr <antwauntune25@wcpsstudent.org>
2016-04-08 11:17:56 -0400

    Update rockps.java

--- rockps.java
+++ rockps.java
@@ -1,6 +1,19 @@
-import java.util.Random
+import java.util.Random;
+import java.util.Scanner;
+
 class RockPaperScisors {
+    public static int rpsRandom(int[] array) {
+        int rand = new Random().nextInt(array.length);
+        return array[rand];
+    }
     public static void main(String args[]) {
-    
+        int[] computer_choices = {0,1,2};
+        int computer_choice = rpsRandom(computer_choices);
+        /*switch (computer_choice) {
+            case 0: if(user_choice)
+            
+            
+        } */
+        System.out.println(computer_choice);
     }
 }
